1cd10bdf1716f370a406929610f83534decec393
SukritiR2909 <sukriti.rajora@wizehive.com>
2024-08-07 12:28:12 +0530

    cancle event test

---
+++ src/tests/Volunteer/cancleEvent.spec.ts
@@ -0,0 +1,21 @@
+import { expect } from "@playwright/test"
+import { test } from '../../fixtures';
+
+test.describe("Volunteer dashboard", async() => {
+    test('Create volunteer event', async({
+        activeAdminPage,
+        adminDashboardPage,
+        bfLandingPage,
+        volunteerMenuPage,
+        createEventPage
+    }) => {
+        await activeAdminPage.goToActiveAdmin();
+        await activeAdminPage.volunteerSettings();
+        await activeAdminPage.adminLogin();
+        await bfLandingPage.goToAdminDashboard();
+        await adminDashboardPage.volunteerSetting();
+        await bfLandingPage.clickMyVolunteer();
+        await volunteerMenuPage.clickEvent();
+        await createEventPage.cancleEvent();
+    })
+})
